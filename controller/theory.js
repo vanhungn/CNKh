3b594e4087aba9c45fae902227864529d41e43a2
vanhungn <vanhungnvh1712004@gmail.com>
2025-11-21 13:54:17 +0700

    ok

---
+++ controller/theory.js
@@ -0,0 +1,36 @@
+const modalTheory = require("../modal/thoery")
+const mongoose = require("mongoose");
+const GetTheory = async (req, res) => {
+    try {
+        const { _id } = req.params
+        const skip = parseInt(req.query.skip) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const search = req.query.search || ""
+        const data = await modalTheory.aggregate([
+            {
+                $match: {
+                    $and: [
+                        { idCourse: new mongoose.Types.ObjectId(_id) },
+                        {
+                            $or: [
+                                { chapter: { $regex: search, $options: "i" } }
+                            ]
+                        }
+                    ]
+                }
+            },
+            { $skip: (skip - 1) * limit },
+            { $limit: limit }
+        ])
+        const dataChapter = []
+        data.forEach((e) => {
+            dataChapter.push({ chapter: e.chapter, _id: e._id })
+        })
+        return res.status(200).json({
+            data: dataChapter
+        })
+    } catch (error) {
+        return res.status(500).json({ error })
+    }
+}
+module.exports = { GetTheory }
--- routes/theory.js
+++ routes/theory.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const theory = require('../controller/practice')
 const verifyToken = require('../middleware/verifyToken')
+const theoryAdmin = require('../controller/theory')
 
-
+router.get('/:_id',theoryAdmin.GetTheory)
+//theory
 router.get('/chapter',verifyToken, theory.GetTheoryChapter)
 router.get('/list/:_id',verifyToken, theory.GetTheoryList)
 router.post('/create',verifyToken, theory.CreateTheory)
