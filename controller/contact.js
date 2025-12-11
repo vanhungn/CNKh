8ba72c33d005bcf7954616f794b85369b4197d52
vanhungn <vanhungnvh1712004@gmail.com>
2025-12-11 10:04:49 +0700

    ok

--- controller/contact.js
+++ controller/contact.js
@@ -9,7 +9,7 @@ const CreateContact = async (req, res) => {
         }
         const create = await modelContact.create({ name, phone, email, title, content })
         return res.status(200).json({
-            message:"successfully",
+            message: "successfully",
             create
         })
     } catch (error) {
@@ -18,5 +18,49 @@ const CreateContact = async (req, res) => {
         })
     }
 }
-
-module.exports ={CreateContact}
+const GetContact = async (req, res) => {
+    try {
+        const sort = parseInt(req.query.sort) || -1
+        const search = req.query.search || ""
+        const skip = parseInt(req.query.skip) || 1
+        const limit = parseInt(req.query.limit) || 10
+        const query = {
+            $match: {
+                $or: [
+                    { title: { $regex: search, $options: "i" } },
+                    { name: { $regex: search, $options:"i"} }
+                ]
+            }
+        }
+        const data = await modelContact.aggregate([query, {
+            $sort: {createdAt:sort}
+        },
+            { $skip: (skip - 1) * limit },
+            { $limit: limit }
+        ])
+        const dataLength = await modelContact.aggregate([query])
+        const total = Math.ceil(dataLength.length / limit)
+        return res.status(200).json({
+            data,total
+        })
+    } catch (error) {
+        return res.status(500).json({ error })
+    }
+}
+const DeleteContact = async(req,res)=>{
+    try {
+        const {_id} = req.params
+        if(!_id){
+            return res.status(400).json({
+                message:"not valid"
+            })
+        }
+        await modelContact.findByIdAndDelete(_id)
+        return res.status(200).json({
+            message:"successFully"
+        })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+module.exports = { CreateContact,GetContact,DeleteContact }
--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const contact = require('../controller/contact')
 
+router.get('/', contact.GetContact)
 router.post('/create', contact.CreateContact)
+router.delete('/delete/:_id',contact.DeleteContact)
 
 module.exports = router
