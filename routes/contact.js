697146f3103f6a02db3bdb4778744b9c1a47249e
vanhungn <vanhungnvh1712004@gmail.com>
2025-12-11 11:37:30 +0700

    ok

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const contact = require('../controller/contact')
+const verify = require('../middleware/verifyToken')
 
-router.get('/', contact.GetContact)
-router.post('/create', contact.CreateContact)
-router.delete('/delete/:_id',contact.DeleteContact)
+router.get('/', verify,contact.GetContact)
+router.post('/create',verify,contact.CreateContact)
+router.delete('/delete/:_id',verify,contact.DeleteContact)
 
 module.exports = router
