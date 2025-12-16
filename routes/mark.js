d654a633b7317ab18bde9b946c74d845d71669e3
vanhungn <vanhungnvh1712004@gmail.com>
2025-12-16 16:21:45 +0700

    ok

--- routes/mark.js
+++ routes/mark.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const mark = require('../controller/mark')
 
-router.get('/', mark.GetMark)
+router.get('/:_id', mark.GetMark)
 router.post('/update/:_id', mark.UpdateMark)
 router.post('/create', mark.CreateMark)
-router.delete('/delete/:_id',mark.DeleteMark)
+router.delete('/delete/:_id', mark.DeleteMark)
 
 module.exports = router
